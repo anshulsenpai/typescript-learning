ff8e505798ead064e8fbd4d2c7e5f3a9f69dce85
Anshul Kulkarni <anshulkulkarni00@gmail.com>
2023-02-11 10:50:47 +0530

    generic constraints & generic classes

--- src/myGenerics.ts
+++ src/myGenerics.ts
@@ -1,46 +1,76 @@
-const scroe: Array<number> = [1, 2, 3]
-const names: Array<string> = ["anshul", "shantanu", "akash"]
+const scroe: Array<number> = [1, 2, 3];
+const names: Array<string> = ["anshul", "shantanu", "akash"];
 
-function identityOne(val : number | boolean): number | boolean {
-    return val
-    //? input <number> can returns <boolean> 
-
+function identityOne(val: number | boolean): number | boolean {
+  return val;
+  //? input <number> can returns <boolean>
 }
 
 // function identityTwo(val : any): any {
 //     return val //! Bad Practice
-    //? input <number> returns <string> 
-    // ? input <any> can return <any>
+//? input <number> returns <string>
+// ? input <any> can return <any>
 
 // }
 
-function identityThree<Type>(val : Type) : Type {
-    return val
-    //? input <number> returns <number> 
+function identityThree<Type>(val: Type): Type {
+  return val;
+  //? input <number> returns <number>
 }
-
 
 interface Drink {
-    brand : string,
-    price : number
+  brand: string;
+  price: number;
 }
 
-function identityFour<Drink>(val : Drink) : Drink {
-    return val
+function identityFour<Drink>(val: Drink): Drink {
+  return val;
 }
 
-const juice : Drink = {
-    brand : "Sprite",
-    price : 50
+const juice: Drink = {
+  brand: "Sprite",
+  price: 50,
+};
+
+identityFour(juice);
+
+//? Generic Arrow Method
+
+const getMoreSearchProducts = <T>(products: T[]): T => {
+  const index = 4;
+  return products[index];
+};
+
+//* Using Type Parameters in Generic Constraint
+
+interface db {
+  connection: string;
+  username: string;
+  password: string;
 }
 
-identityFour(juice)
+function anotherFun<T, U extends db>(valOne: T, valTwo: U): object {
+  return {
+    valOne,
+    valTwo,
+  };
+}
+
+anotherFun(1, {
+  connection: "mongoURL",
+  username: "anshulMongo",
+  password: "anshul@mongo",
+});
 
 
-//? Generic Arrow Method 
 
-const getMoreSearchProducts = <T,>(products : T[]): T => {
-    const index = 4
-    return products[index]
+//? Generic Classes
+
+class MyClass<T> {
+    public cart: T[] = []
+
+    addToCart(prods: T) {
+        this.cart.push(prods)
+    }
 }
 
